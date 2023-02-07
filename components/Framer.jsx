349b588df689ca41589e4a0ea1cc62f1a242ee71
Prasad48432 <prasadreddymar6@gmail.com>
2023-02-07 10:28:51 +0530

    commited to build

--- components/Framer.jsx
+++ components/Framer.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { motion, AnimatePresence } from "framer-motion";
+import { motion} from "framer-motion";
 import Image from "next/image";
 
 const Framer = () => {
