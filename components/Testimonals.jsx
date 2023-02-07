468eaa9c47a3de400a340e088e2ebc5a27c099a6
Prasad48432 <prasadreddymar6@gmail.com>
2023-02-07 20:13:56 +0530

    changed error

--- components/Testimonals.jsx
+++ components/Testimonals.jsx
@@ -4,55 +4,55 @@ const Testimonals = () => {
   return (
     <>
       <div className="testimonals ">
-          <div class="block p-6 rounded-lg shadow-lg  w-full bg-[#6096B4]">
-            <h5 class="text-gray-900 text-xl leading-tight font-medium mb-2">
+          <div className="block p-6 rounded-lg shadow-lg  w-full bg-[#6096B4]">
+            <h5 className="text-gray-900 text-xl leading-tight font-medium mb-2">
               Card title 1
             </h5>
-            <p class="text-gray-700 text-base mb-4">
+            <p className="text-gray-700 text-base mb-4">
               Some quick example text to build on the card title and make up the
-              bulk of the card's content.
+              bulk of the cards content.
               Lorem ipsum dolor sit amet consectetur adipisicing elit. 
               Praesentium expedita ut corporis magnam voluptates saepe itaque tenetur perferendis quis quas,
                ullam rem sit nisi similique quisquam! Dolor doloremque blanditiis amet?
             </p>
           </div>
-        <div class="block p-6 rounded-lg shadow-lg  w-full bg-[#93BFCF]">
-            <h5 class="text-gray-900 text-xl leading-tight font-medium mb-2">
+        <div className="block p-6 rounded-lg shadow-lg  w-full bg-[#93BFCF]">
+            <h5 className="text-gray-900 text-xl leading-tight font-medium mb-2">
               Card title 2
             </h5>
-            <p class="text-gray-700 text-base mb-4">
+            <p className="text-gray-700 text-base mb-4">
               Some quick example text to build on the card title and make up the
-              bulk of the card's content.
+              bulk of the cards content.
             </p>
           </div>
-        <div class="block p-6 rounded-lg shadow-lg  w-full bg-[#BDCDD6]">
-            <h5 class="text-gray-900 text-xl leading-tight font-medium mb-2">
+        <div className="block p-6 rounded-lg shadow-lg  w-full bg-[#BDCDD6]">
+            <h5 className="text-gray-900 text-xl leading-tight font-medium mb-2">
               Card title 3
             </h5>
-            <p class="text-gray-700 text-base mb-4">
+            <p className="text-gray-700 text-base mb-4">
               Some quick example text to build on the card title and make up the
-              bulk of the card's content.
+              bulk of the cards content.
               Lorem ipsum dolor sit amet consectetur adipisicing elit. 
             </p>
           </div>
-        <div class="block p-6 rounded-lg shadow-lg  w-full bg-[#EEE9DA]">
-            <h5 class="text-gray-900 text-xl leading-tight font-medium mb-2">
+        <div className="block p-6 rounded-lg shadow-lg  w-full bg-[#EEE9DA]">
+            <h5 className="text-gray-900 text-xl leading-tight font-medium mb-2">
               Card title 4
             </h5>
-            <p class="text-gray-700 text-base mb-4">
+            <p className="text-gray-700 text-base mb-4">
               Some quick example text to build on the card title and make up the
-              bulk of the card's content.
+              bulk of the cards content.
               Lorem ipsum dolor sit amet consectetur adipisicing elit. 
               Praesentium expedita ut corporis magnam voluptates saepe itaque tenetur perferendis quis quas,
             </p>
           </div>
-        <div class="block p-6 rounded-lg shadow-lg  w-full bg-[#F8CBA6]">
-            <h5 class="text-gray-900 text-xl leading-tight font-medium mb-2">
+        <div className="block p-6 rounded-lg shadow-lg  w-full bg-[#F8CBA6]">
+            <h5 className="text-gray-900 text-xl leading-tight font-medium mb-2">
               Card title 5
             </h5>
-            <p class="text-gray-700 text-base mb-4">
+            <p className="text-gray-700 text-base mb-4">
               Some quick example text to build on the card title and make up the
-              bulk of the card's content.
+              bulk of the cards content.
               Lorem ipsum dolor sit amet consectetur adipisicing elit. 
               Praesentium expedita ut corporis magnam voluptates saepe itaque tenetur perferendis quis quas,
                ullam rem sit nisi similique quisquam! Dolor doloremque blanditiis amet?
