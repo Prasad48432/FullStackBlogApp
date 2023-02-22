90a33e0ebeb62fb0e696e61aa915beb85e4209fa
Prasad48432 <prasadreddymar6@gmail.com>
2023-02-22 21:50:10 +0530

    changed ui

--- components/HoverGallery.jsx
+++ components/HoverGallery.jsx
@@ -6,8 +6,9 @@ const HoverGallery = () => {
     <>
       <div className="swiper-container">
         <div className="card-swiper">
-            <img src="./handarrow.jpg" alt="arrow" className='w-[45px] h-[45px] relative bottom-5 left-5'/>
-        <h1 className='text-3xl text-black text-right relative bottom-20' >Hover me</h1>
+            <img src="./handarrow.jpg" alt="arrow" className='w-[45px] h-[45px] relative sm:bottom-5 sm:left-5 right-12 rotate-[-12deg] bottom-3'/>
+        <h1 className='text-right relative bottom-20 hidden sm:block awesome_gradient text-3xl' >Hover me</h1>
+        <h1 className='text-3xl text-black text-right relative bottom-20 block sm:hidden awesome_gradient' >Press me</h1>
           <div className="card-groups">
             <div className="card-group" data-index="0" data-status="active">
               <div className="little-card card"></div>
