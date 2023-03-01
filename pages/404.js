b4135f0434cfe3d27fdabc4ac2931ec774e5fa21
Prasad48432 <prasadreddymar6@gmail.com>
2023-03-01 16:23:37 +0530

    corrected errors

--- pages/404.js
+++ pages/404.js
@@ -5,43 +5,43 @@ import React from "react";
 const ErrorPage = () => {
   return (
     <>
-      <div class="flex items-center justify-center min-h-screen bg-white dark:bg-[#182a46] py-[7rem]">
-        <div class="flex flex-col">
-          <div class="flex flex-col items-center">
-            <div class="">
+      <div className="flex items-center justify-center min-h-screen bg-white dark:bg-[#182a46] py-[7rem]">
+        <div className="flex flex-col">
+          <div className="flex flex-col items-center">
+            <div className="">
                 <img src='https://miro.medium.com/v2/resize:fit:828/1*qdFdhbR00beEaIKDI_WDCw.gif' className="W-[300px] h-[200px] object-cover" alt='404' />
             </div>
 
-            <div class="font-bold text-2xl xl:text-4xl lg:text-6xl md:text-5xl mt-10">
+            <div className="font-bold text-2xl xl:text-4xl lg:text-6xl md:text-5xl mt-10">
               This page does not exist
             </div>
 
-            <div class="text-gray-400 font-medium text-sm md:text-xl lg:text-2xl mt-8">
+            <div className="text-gray-400 font-medium text-sm md:text-xl lg:text-2xl mt-8">
               The page you are looking for could not be found.
             </div>
           </div>
 
-          <div class="flex flex-col mt-2">
-            <div class="text-gray-400 font-bold uppercase">Continue With</div>
+          <div className="flex flex-col mt-2">
+            <div className="text-gray-400 font-bold uppercase">Continue With</div>
 
-            <div class="flex flex-row items-stretch mt-5">
+            <div className="flex flex-row items-stretch mt-5">
             <Link href="/">
               <div
-                class="flex flex-row group px-4 py-8
+                className="flex flex-row group px-4 py-8
                     border-t hover:cursor-pointer
                     transition-all duration-200 delay-100"
               >
-                <div class="rounded-xl bg-blue-100 p-2 md:py-4">
+                <div className="rounded-xl bg-blue-100 p-2 md:py-4">
                   <span className="text-black" ><FiHome size={30}/></span>
                 </div>
 
-                <div class="grow flex flex-col pl-5 pt-2">
-                  <div class="font-bold text-sm md:text-lg lg:text-xl group-hover:underline">
+                <div className="grow flex flex-col pl-5 pt-2">
+                  <div className="font-bold text-sm md:text-lg lg:text-xl group-hover:underline">
                     Home 
                   </div>
 
                   <div
-                    class="font-semibold text-sm md:text-md lg:text-lg
+                    className="font-semibold text-sm md:text-md lg:text-lg
                             text-gray-400 group-hover:text-gray-500
                             transition-all duration-200 delay-100"
                   >
@@ -52,21 +52,21 @@ const ErrorPage = () => {
               </Link>
               <Link href="/blogs">
               <div
-                class="flex flex-row group px-4 py-8
+                className="flex flex-row group px-4 py-8
                     border-t hover:cursor-pointer
                     transition-all duration-200 delay-100"
               >
-                <div class="rounded-xl bg-blue-100 p-2 md:py-4">
+                <div className="rounded-xl bg-blue-100 p-2 md:py-4">
                   <span className="text-black" ><FiFileText size={30}/></span>
                 </div>
 
-                <div class="grow flex flex-col pl-5 pt-2">
-                  <div class="font-bold text-sm md:text-lg lg:text-xl group-hover:underline">
+                <div className="grow flex flex-col pl-5 pt-2">
+                  <div className="font-bold text-sm md:text-lg lg:text-xl group-hover:underline">
                     Blogs
                   </div>
 
                   <div
-                    class="font-semibold text-sm md:text-md lg:text-lg
+                    className="font-semibold text-sm md:text-md lg:text-lg
                             text-gray-400 group-hover:text-gray-500
                             transition-all duration-200 delay-100"
                   >
@@ -77,21 +77,21 @@ const ErrorPage = () => {
               </Link>
               <Link href='/#gallery'>
               <div
-                class="flex flex-row group px-4 py-8
+                className="flex flex-row group px-4 py-8
                     border-t hover:cursor-pointer
                     transition-all duration-200 delay-100"
               >
-                <div class="rounded-xl bg-blue-100 p-2 md:py-4">
+                <div className="rounded-xl bg-blue-100 p-2 md:py-4">
                   <span className="text-black"><FiImage size={30}/></span>
                 </div>
 
-                <div class="grow flex flex-col pl-5 pt-2">
-                  <div class="font-bold text-sm md:text-lg lg:text-xl group-hover:underline">
+                <div className="grow flex flex-col pl-5 pt-2">
+                  <div className="font-bold text-sm md:text-lg lg:text-xl group-hover:underline">
                     Gallery
                   </div>
 
                   <div
-                    class="font-semibold text-sm md:text-md lg:text-lg
+                    className="font-semibold text-sm md:text-md lg:text-lg
                             text-gray-400 group-hover:text-gray-500
                             transition-all duration-200 delay-100"
                   >
@@ -102,21 +102,21 @@ const ErrorPage = () => {
               </Link>
             <Link href="/contact">
               <div
-                class="flex flex-row group px-4 py-8
+                className="flex flex-row group px-4 py-8
                     border-t hover:cursor-pointer
                     transition-all duration-200 delay-100"
               >
-                <div class="rounded-xl bg-blue-100 p-2 md:py-4">
+                <div className="rounded-xl bg-blue-100 p-2 md:py-4">
                   <span className="text-black" ><FiMessageCircle size={30}/></span>
                 </div>
 
-                <div class="grow flex flex-col pl-5 pt-2">
-                  <div class="font-bold text-sm md:text-lg lg:text-xl group-hover:underline">
+                <div className="grow flex flex-col pl-5 pt-2">
+                  <div className="font-bold text-sm md:text-lg lg:text-xl group-hover:underline">
                     Contact
                   </div>
 
                   <div
-                    class="font-semibold text-sm md:text-md lg:text-lg
+                    className="font-semibold text-sm md:text-md lg:text-lg
                             text-gray-400 group-hover:text-gray-500
                             transition-all duration-200 delay-100"
                   >
