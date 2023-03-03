53fcfdaf412e37d1db33d3778b80007013ea09d8
Prasad48432 <prasadreddymar6@gmail.com>
2023-03-03 08:52:43 +0530

    changed ui

--- pages/_document.js
+++ pages/_document.js
@@ -8,7 +8,7 @@ export default function Document() {
         <link rel="preconnect" href="https://fonts.gstatic.com" />
         <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet"/>
       </Head>
-      <body className='dark:bg-[#0b192f] bg-white '>
+      <body className='dark:bg-[#0b192f] bg-white'>
         <Main />
         <NextScript />
       </body>
